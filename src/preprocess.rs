a0d351070ffa2f5b3da1bb515424f9749bb83ea8
Steven Le <stevenhle459@gmail.com>
2021-11-08 15:51:49 -0600

    Init commit

---
+++ src/preprocess.rs
@@ -0,0 +1,17 @@
+pub mod preprocess {
+    struct Data {
+        url: str;
+        data: Vec<str[]>;
+    }
+
+    impl Data {
+        fn init(url: str) -> Data {
+            self.url = url;
+            self.data = Vec::new();
+        }
+
+        fn read() {
+            
+        }
+    }
+}
